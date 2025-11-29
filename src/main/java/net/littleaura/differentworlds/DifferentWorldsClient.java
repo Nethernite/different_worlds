5847def091024c3838ae106c960619ae36dd7c8c
Nethernite <juri.guguran@gmail.com>
2025-11-29 15:53:51 +0100

    Initial commit

---
+++ src/main/java/net/littleaura/differentworlds/DifferentWorldsClient.java
@@ -0,0 +1,10 @@
+package net.littleaura.differentworlds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DifferentWorldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
